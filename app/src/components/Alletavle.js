b63619664885dc2908802b72ab85243746daa49a
Matias Opsahl <matiasop@stud.ntnu.no>
2023-03-19 17:40:35 +0100

    Completed search functionality

--- app/src/components/Alletavle.js
+++ app/src/components/Alletavle.js
@@ -8,10 +8,10 @@ function sortByTotal(data) {
 }
 
 const Alletavle = (props) => {
+  
   const [inputValue, setInputValue] = useState("");
-  const data = sortByTotal(props.data);
+  const data = sortByTotal(props.data).map((item, index) => ({ ...item, plassering: index + 1 }));
 
-  // Filter the data array based on the input value
   const filteredData = data.filter((row) =>
     row.firstname.toLowerCase().includes(inputValue.toLowerCase())
   );
@@ -19,14 +19,13 @@ const Alletavle = (props) => {
   const rows = filteredData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{index + 1}</td>
+        <td>{row.plassering}</td>
         <td>{row.firstname}</td>
         <td>{row.total}</td>
       </tr>
     );
   });
 
-  const [focused, setFocused] = useState(false);
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
@@ -37,8 +36,6 @@ const Alletavle = (props) => {
       type="text" value={inputValue} onChange={handleInputChange}
       label="Søk etter ditt navn"
       placeholder="Søk"
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
       />
     
       <br />
