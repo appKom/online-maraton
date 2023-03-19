9a5260a50d4aab7b1bc1c0da7e8177a2b1751905
Sander Linnerud <linnerudsander@gmail.com>
2023-03-19 16:29:07 +0100

    sortert tabell, mangler søkefunksjon

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import HeaderResponsive from './components/Header';
+import "./App.css";
+import HeaderResponsive from "./components/Header";
+import Alletavle from "./components/Alletavle";
 
 function App() {
-
   const links = [
-    { label: 'Sponsorer', link: '/' },
-    { label: 'Om arrangementet', link: '/' },
-    { label: 'Ledertavle', link: '/' },
+    { label: "Sponsorer", link: "/" },
+    { label: "Om arrangementet", link: "/" },
+    { label: "Ledertavle", link: "/" },
+  ];
+
+  const alletavleData = [
+    { firstname: "Sander", lastname: "L.", total: "123" },
+    { firstname: "Matias", lastname: "O.", total: "124" },
+    { firstname: "David", lastname: "L.", total: "125" },
+    { firstname: "Julian", lastname: "O.", total: "126" },
+    { firstname: "Sander", lastname: "L.", total: "123" },
+    { firstname: "Matias", lastname: "O.", total: "124" },
+    { firstname: "David", lastname: "L.", total: "125" },
+    { firstname: "Julian", lastname: "O.", total: "126" },
+    { firstname: "Sander", lastname: "L.", total: "123" },
+    { firstname: "Matias", lastname: "O.", total: "124" },
+    { firstname: "David", lastname: "L.", total: "125" },
+    { firstname: "Julian", lastname: "O.", total: "126" },
+    { firstname: "Sander", lastname: "L.", total: "123" },
+    { firstname: "Matias", lastname: "O.", total: "124" },
+    { firstname: "David", lastname: "L.", total: "125" },
+    { firstname: "Julian", lastname: "O.", total: "126" },
+    { firstname: "Sander", lastname: "L.", total: "123" },
+    { firstname: "Matias", lastname: "O.", total: "124" },
+    { firstname: "David", lastname: "L.", total: "125" },
+    { firstname: "Julian", lastname: "O.", total: "126" },
   ];
 
   return (
-    <HeaderResponsive links={links}/>
+    <div>
+      <HeaderResponsive links={links} />
+      <Alletavle data={alletavleData} />
+    </div>
   );
 }
 
