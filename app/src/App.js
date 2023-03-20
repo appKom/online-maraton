e98f2fc17a9365d13040a5e3b6fe34efc9a5c565
Julian <julianao@stud.ntnu.no>
2023-03-20 09:33:25 +0100

    added coverimages, paragraphs, and temporary sponsors

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import HeaderResponsive from './components/Header';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sponsors from "./components/Sponsors";
+import Paragraph from "./components/Paragraph";
+import CoverImage from "./components/CoverImage";
+import LederTavle from "./components/LederTavle";
+import AlleTavle from "./components/AlleTavle";
 
 function App() {
-
-  const links = [
-    { label: 'Sponsorer', link: '/' },
-    { label: 'Om arrangementet', link: '/' },
-    { label: 'Ledertavle', link: '/' },
-  ];
-
   return (
-    <HeaderResponsive links={links}/>
+    <div>
+      <Header />
+      <CoverImage image="/temporary-cover1.jpg" />
+
+      <Sponsors />
+      <Paragraph
+        header="Hvorfor løper vi?"
+        text="Barnekreft ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Barnekreft ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      "
+      />
+
+      <CoverImage image="/temporary-cover2.jpg" />
+
+      <Paragraph
+        header="Hvordan løper vi?"
+        text="Strava ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      "
+      />
+
+      <LederTavle />
+
+      <AlleTavle />
+
+      <Footer />
+    </div>
   );
 }
 
---
+++ app/src/components/AlleTavle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/main.css";
+
+function AlleTavle(props) {
+  return (
+    <div>
+        Alletavle
+    </div>
+  );
+}
+
+export default AlleTavle;
