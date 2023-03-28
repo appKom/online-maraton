59e6dbf21bced5028485e9cdf3cbbc47c12cd7db
Jørgen Galdal <jorggal@stud.ntnu.no>
2023-03-28 12:04:51 +0200

    La til tall til løpebane

--- app/src/components/JoggaSpesial.jsx
+++ app/src/components/JoggaSpesial.jsx
@@ -10,15 +10,18 @@ const JoggaSpesial = () => {
     const viewboxHeight = trackTop + grassHeight * numOfRunners
     const viewboxWidth = 300
     const firstplaceX = viewboxWidth - (nodeRadius) - 1
+    const fontSize = 16
     return (
         <div>
-            <svg id="e15LCYrABvo1" xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${viewboxWidth} ${viewboxHeight}`}>
+            <svg id="e15LCYrABvo1" xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${viewboxWidth} ${viewboxHeight}`} lineHeight={`${fontSize}px`}>
 
                 {[...Array(6).keys()].map((i) =>
                     <g>
                         {/* Track fields: */}
                         <rect width="300" height={`${grassHeight}`} transform={`translate(0 ${trackTop + i * grassHeight})`} fill="#dd1111" stroke-width="1" stroke="#FFF" />
-                        {/* <line width/> */}
+                        <rect width="2" height={`${grassHeight - 6}`} transform={`translate(${grassHeight} ${trackTop + i * grassHeight + 3})`} fill="#FFFFFF"/>
+
+                        <text transform={`translate(${5} ${trackTop + (i+1) * grassHeight - 4.4})`} fill="#FFF">{`${i + 1}`}</text>
 
                         {/* Markers */}
                         <ellipse rx={nodeRadius} ry={nodeRadius} fill="#d2dbed" stroke="#000" transform={`translate(${((i + 1) / numOfRunners) * firstplaceX} ${trackTop + i * grassHeight})`} />
