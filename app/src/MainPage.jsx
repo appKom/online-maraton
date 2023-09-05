764d5e84d301cb16f3ba662a634ef809df953c09
Julian <julianao@stud.ntnu.no>
2023-09-05 18:04:53 +0200

    Vippsnummer separert fra avsnitt

--- app/src/MainPage.jsx
+++ app/src/MainPage.jsx
@@ -5,6 +5,7 @@ import CoverImage from "./components/CoverImage";
 import AlleTavle from "./components/AlleTavle";
 import JoggaSpesial from "./components/JoggaSpesial";
 import { useState, useEffect } from "react";
+import "./styles/main.css";
 
 const MainPage = (props) => {
   const [stravaData, setStravaData] = useState([]);
@@ -38,7 +39,7 @@ const MainPage = (props) => {
         header="Hvorfor løper vi?"
         text="Barnekreft er ikke det samme som kreft hos voksne. Hos barn utvikles kreften hurtig
         og det finnes ingen kjente ytre faktorer som er av betydning for hvordan sykdommen utvikler seg.
-        Rundt 200 barn og unge mellom 0 til 18 år rammes hvert år av en kreftsykdom. Totaloverlevelsen
+        Rundt 200 barn og unge mellom 0 til 18 år rammes hvert år av en kreftsykdom i Norge. Totaloverlevelsen
         av barnekreft er i Norge er cirka 85 prosent.
 
         Selv om kreft hos barn og ungdom heldigvis er sjelden, er man desto mer avhengig av både nasjonalt,
@@ -58,7 +59,7 @@ const MainPage = (props) => {
         x antall kroner per onliner som fullfører x antall kilometer.
 
         Vi bruker Stava for å hente info om hvor langt hver deltaker har løpt. Du må derfor opprette en bruker på
-        Strava på www.strava.com dersom du ikke har det fra før.
+        Strava dersom du ikke har det fra før.
 
         For å delta i innsamlingen som løper, melder du deg på via MELD DEG PÅ-knappen øverst på siden.
       "
@@ -67,9 +68,17 @@ const MainPage = (props) => {
       <div id="støtt" >
         <Paragraph
         header="Støtt barnekreftforeningen"
-        text="Du har nå muligheten til å støtte Barnekreftforeningen under løpeuken ved å vippse en donasjon. Hver eneste krone teller, og din sjenerøse støtte vil bidra til å forbedre livene til barn som kjemper mot kreft og deres familier. Sammen kan vi utgjøre en forskjell og gi håp til de som trenger det mest. Takk for din støtte!
-        Vippsnummer: XXX"
+        text="Du har nå muligheten til å støtte Barnekreftforeningen under løpeuken ved å vippse en donasjon. Hver eneste krone teller, og din sjenerøse støtte vil bidra til å forbedre livene til barn som kjemper mot kreft og deres familier. Sammen kan vi utgjøre en forskjell og gi håp til de som trenger det mest. Takk for din støtte!"
         />
+
+        <div className="paragraph-main">
+            <div className="paragraph-submain">
+                <div className="paragraph-text">
+                    Vippsnummer: <b>XXX</b>
+                </div>
+            </div>
+        </div>
+
       </div>
 
       <div id="ledertavle">
